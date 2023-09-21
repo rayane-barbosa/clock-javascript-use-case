110bcc7b4331f61963f86bb181d3a26a4147dafe
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-09-21 21:42:42 +0000

    update: update component Counter

--- digital-clock/src/components/counter.tsx
+++ digital-clock/src/components/counter.tsx
@@ -2,10 +2,13 @@
 import React, { useState } from "react";
 
 export default function Counter(){
-    const [count, setCount] = useState(new Date());
+    const [time, setTime] = useState(new Date());
 
-    setTimeout(() => { setCount(new Date()) }, 1000);
+    const updateClock = () => setTime(new Date());
 
 
-    return <>{count.toTimeString()}</>;
+    setTimeout(() => { updateClock() }, 1000);
+
+
+    return <>{time.toLocaleTimeString()}</>;
 }
